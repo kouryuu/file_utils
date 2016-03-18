10ea26f83d8fa7054b0236eda9ca09f786433e33
rreyes <rodrigo@plethora.com.mx>
2016-03-17 20:21:31 -0600

    Added some doc

--- file_parse.py
+++ file_parse.py
@@ -1,11 +1,17 @@
-# Program to do file juggling
-import sys
+"""" Program to do file juggling
+    made by Rodrigo R
+    it depends on delimiter_definitions to define the row-based and the field based operations
+
+"""
+import sys # to read argv from command line
 import delimiter_definitions as dd
+
 in_file = open(sys.argv[1])
 in_file_str = in_file.read()
 in_file_array = in_file_str.split()
-delimiter = '|'
-out_file = open(sys.argv[2])
+
+delimiter = '|' #should this be hardcoded?
+out_file = open(sys.argv[2]) #should I create it if it does not exist?
 
 
 for i in range(0,len(in_file_array)):
