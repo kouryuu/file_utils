beb0bb65ad5217362266c5bcfc111ba23cca8fb2
kouryuu <antarticfreeze@gmail.com>
2016-03-17 23:10:48 -0600

    Improved documentation

--- file_parse.py
+++ file_parse.py
@@ -1,4 +1,4 @@
-"""" Program to do file juggling
+"""" Program to do file parsing
     made by Rodrigo R
     it depends on delimiter_definitions to define the row-based and the field based operations
 
@@ -6,16 +6,16 @@
 import sys # to read argv from command line
 import delimiter_definitions as dd #You should define your own delimiter definitions.
 
-in_file = open(sys.argv[1])
+in_file = open(sys.argv[1],'r')
 in_file_str = in_file.read()
 in_file_array = in_file_str.split()
 
-delimiter = '|' #should this be hardcoded?
-out_file = open(sys.argv[2]) #should I create it if it does not exist?
+delimiter = ',' #should this be hardcoded?
+out_file = open(sys.argv[2],'w') #should I create it if it does not exist?
 
 
 for i in range(0,len(in_file_array)):
     delimiter_separated = in_file_array[i].split(delimiter)
     dd.do_with_delimiter_array(delimiter_separated)
 print dd.out_string
-
+out_file.write(dd.out_string)
