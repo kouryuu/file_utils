fe8041edf1e4094167aca7f1bb7471d8e3a5126b
rreyes <rodrigo@plethora.com.mx>
2016-03-17 20:32:36 -0600

    Improved things need to fix global variable

--- delimiter_definitions.py
+++ delimiter_definitions.py
@@ -1,8 +1,10 @@
+global out_string 
+
 def do_with_delimiter_array(array):
     for i in range(0,len(array)):
         do_with_delimiter_field(array[i],i,array)
 
 def do_with_delimiter_field(field,column,array):
-    print str(column) + " . "+str(field)
+    out_string = out_string + str(field) + "-" 
 
 
--- file_parse.py
+++ file_parse.py
@@ -4,7 +4,7 @@
 
 """
 import sys # to read argv from command line
-import delimiter_definitions as dd
+import delimiter_definitions as dd #You should define your own delimiter definitions.
 
 in_file = open(sys.argv[1])
 in_file_str = in_file.read()
@@ -17,5 +17,5 @@ out_file = open(sys.argv[2]) #should I create it if it does not exist?
 for i in range(0,len(in_file_array)):
     delimiter_separated = in_file_array[i].split(delimiter)
     dd.do_with_delimiter_array(delimiter_separated)
-
+print dd.out_string
 
