17fc1d06f09bcb0ecc69eff30bf809fa7fa32cd3
kouryuu <antarticfreeze@gmail.com>
2016-03-30 00:31:38 -0600

    Added delimiter global variable

--- delimiter_definitions.py
+++ delimiter_definitions.py
@@ -3,15 +3,18 @@
     The example prints the number of the column followed by the content of the field
 """
 out_string = "" # global variable used to encapsulate what will be written to the out file
+delimiter = "\n"
 """ This function deals with the array split by the delimiter it is called as many times as there are lines in the file
 """
 def do_with_delimiter_array(array):
+    print array
     for i in range(0,len(array)):
-        do_with_delimiter_field(array[i],i,array)
+         do_with_delimiter_field(array[i],i,array)
+
 """ This function is auxiliary to deal with field logic
 """
 
 def do_with_delimiter_field(field,column,array):
     global out_string
     #out_string = out_string +str(column)+':'+str(field)+'\n'
-    out_string = out_string +str(field)+'|'
+    out_string = out_string +str(field)
