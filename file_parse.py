5be665598be4d49c2d5d481efef269127b7da617
rreyes <antarticfreeze@gmail.com>
2016-03-30 12:09:54 -0600

    Fixed split() function to only do it newlines

--- file_parse.py
+++ file_parse.py
@@ -17,18 +17,17 @@ else:
     if( sys.argv[1] == sys.argv[2]): #Self output
         in_file = open(sys.argv[1],'r+b')
         in_file_str = in_file.read()
-        in_file_array = in_file_str.split()
+        in_file_array = in_file_str.split('\n')
         out_file = in_file
     else:
         in_file = open(sys.argv[1],'r')
         in_file_str = in_file.read()
-        in_file_array = in_file_str.split()
+        in_file_array = in_file_str.split('\n')
     if(len(sys.argv) == 3): #optionaly give the delimiter
         delimiter = dd.delimiter 
     else:
         delimiter = sys.argv[3]
     out_file = open(sys.argv[2],'w') #should I create it if it does not exist?
-
 
     for i in range(0,len(in_file_array)):
         delimiter_separated = in_file_array[i].split(delimiter)
