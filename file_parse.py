bf2df18eb858424bec661e12be02b59e053b4782
rreyes <antarticfreeze@gmail.com>
2016-03-30 13:15:38 -0600

    Added stdout capabilities by specifying 'stdout' instead of the out-file name.[file_parse.py]

--- file_parse.py
+++ file_parse.py
@@ -11,7 +11,8 @@ import delimiter_definitions as dd #You should define your own delimiter definit
 
 if(len(sys.argv) <= 2): #Too few arguments protection
     print '''You need to specify both in the command line the arguments
-file_parse [in-file] [out-file] (optional)[delimiter]'''
+file_parse [in-file] [out-file] (optional)[delimiter]
+If instead of out-file you specify stdout it will output it to stdout'''
     sys.exit()
 else:
     if( sys.argv[1] == sys.argv[2]): #Self output
@@ -24,15 +25,18 @@ else:
         in_file_str = in_file.read()
         in_file_array = in_file_str.split('\n')
     if(len(sys.argv) == 3): #optionaly give the delimiter
-        delimiter = dd.delimiter 
+        delimiter = dd.delimiter
     else:
         delimiter = sys.argv[3]
-    out_file = open(sys.argv[2],'w') #should I create it if it does not exist?
+    if(sys.argv[2] != 'stdout'):
+        out_file = open(sys.argv[2],'w') #should I create it if it does not exist?
 
     for i in range(0,len(in_file_array)):
         delimiter_separated = in_file_array[i].split(delimiter)
         dd.do_with_delimiter_array(delimiter_separated)
     print dd.out_string
-    out_file.write(dd.out_string)
+    if(sys.argv[2] != 'stdout'):
+        out_file.write(dd.out_string)
+        out_file.close()
     in_file.close()
-    out_file.close()
+    
